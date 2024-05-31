0324eca50a704eb4f9b7186ec174f56b57457dc3
Sri Abhishek Mamidi <66781982+sam5747@users.noreply.github.com>
2024-05-31 20:17:17 +0000

    Changed the code to use Hooks

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,47 @@
 import React, { useState } from "react";
-import "./Carousel.css";
+import "../components/Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
-function Carousel() {
-}
+const Carousel = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1,
+    );
+  };
+
+  const handlePrev = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1,
+    );
+  };
+
+  return (
+    <div className="carousel-container">
+      <button className="prev-btn" onClick={handlePrev}>
+        <ArrowBackIosIcon />
+      </button>
+      <div className="image-container">
+        <img
+          className="carousel-image"
+          src={images[currentImageIndex].img}
+          alt={images[currentImageIndex].title}
+        />
+        <div className="image-text title">
+          <h2>{images[currentImageIndex].title}</h2>
+        </div>
+        <div className="image-text subtitle">
+          <p>{images[currentImageIndex].subtitle}</p>
+        </div>
+      </div>
+      <button className="next-btn" onClick={handleNext}>
+        <ArrowForwardIosIcon />
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
